c46c917efdce4db56cf8eb702d35329084d205c6
Kent Hu <kenthu@kenthu.net>
2021-08-22 17:10:13 -0700

    Go back to single webpack entry point

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: {
-    index: './src/index.js',
-    parser: './src/parser.js',
-  },
+  entry: './src/index.js',
   devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin(),
