e3148e7ba5ce17b1a4db124d6a6d42f70c9e1b79
Kent Hu <kenthu@kenthu.net>
2021-08-31 12:50:39 -0700

    Set up babel

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
       {
         test: /\.(js)$/,
         exclude: /node_modules/,
-        use: ['eslint-loader'],
+        use: ['babel-loader', 'eslint-loader'],
       },
       {
         test: /\.css$/,
