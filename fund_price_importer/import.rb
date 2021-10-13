c8766226e728d852bc4f815799ef55b43858a363
Kent Hu <kenthu@kenthu.net>
2021-10-13 15:18:09 -0700

    Refactor of import.rb

--- fund_price_importer/import.rb
+++ fund_price_importer/import.rb
@@ -16,6 +16,8 @@ FUND_SYMBOLS = %w[CSVIX CVMRX DFISX DFSPX GCVIX LHYVX MPHQX PDGIX TBCIX TROIX TR
 START_TIMESTAMP = 1483228800
 END_TIMESTAMP = Time.now.to_i
 
+OUTPUT_PATH = '../src/prices.json'.freeze
+
 def import_fund_prices(fund, prices)
   # Headers obtained from Firefox dev tools as curl command, then converted using https://jhawthorn.github.io/curl-to-ruby/
   uri = URI.parse("https://query1.finance.yahoo.com/v7/finance/download/#{fund}?period1=#{START_TIMESTAMP}&period2=#{END_TIMESTAMP}&interval=1d&events=history&includeAdjustedClose=true")
@@ -55,7 +57,7 @@ end
 
 # Output as JS object literal
 def write_to_file(prices)
-  File.open('../src/prices.json', 'w') do |f|
+  File.open(OUTPUT_PATH, 'w') do |f|
     f.write(prices.to_json)
   end
 end
