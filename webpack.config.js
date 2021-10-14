ce20d54fd8963ec9682a74f51498c8bf57ce14f3
Kent Hu <kenthu@kenthu.net>
2021-10-13 21:36:36 -0700

    Implement cache-busting

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
     ],
   },
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
