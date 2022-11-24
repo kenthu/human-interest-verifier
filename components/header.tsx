d930338ed60df3c5ef887dde8836fedb512110b2
Kent Hu <kenthu@kenthu.net>
2022-11-24 08:11:14 -0800

    Use Link component

--- components/header.tsx
+++ components/header.tsx
@@ -18,12 +18,12 @@ export default function Header() {
   return (
     <div className="container">
       <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-        <a
-          href="./"
+        <Link
+          href="/"
           className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none"
         >
           <span className="fs-4">Human Interest 401(k) Verifier</span>
-        </a>
+        </Link>
         <ul className="nav nav-pills">
           {pages.map((page) => {
             const onThisPage = page.href === asPath;
