816b2fdeeae9cd4a55df958e4fecac6b2a1d6876
Kent Hu <kenthu@kenthu.net>
2022-11-26 16:14:18 -0800

    Add ESLint plugin for React hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,10 +7,12 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:import/recommended',
     'plugin:import/typescript',
+    'plugin:react-hooks/recommended',
     'prettier',
   ],
   rules: {
     'import/order': ['error', { 'newlines-between': 'always', alphabetize: { order: 'asc' } }],
+    'react-hooks/exhaustive-deps': 'error',
   },
   settings: {
     'import/resolver': {
