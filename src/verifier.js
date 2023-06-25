43e82f1522ba649354e03747cb3fee417af0caf4
Kent Hu <kenthu@kenthu.net>
2023-06-25 14:02:40 -0700

    Remove legacy Sentry code

--- src/verifier.js
+++
@@ -1,8 +0,0 @@
-import * as Sentry from '@sentry/browser';
-import { Integrations } from '@sentry/tracing';
-
-Sentry.init({
-  dsn: 'https://075ffd56f010481db91c9fb856ef73f0@o1029288.ingest.sentry.io/5996228',
-  integrations: [new Integrations.BrowserTracing()],
-  tracesSampleRate: 1.0,
-});
